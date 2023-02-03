63e619eaea4530e9a2836389862f1d89fe8e76a1
Jessy Lachapelle <jessy.lachapelle@akinox.com>
2023-02-03 15:29:13 +0700

    Update location

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,6 @@ import { Locale } from "../types";
 
 export const DEFAULT_LOCALE = "en" as Locale;
 
-export const END_DATE = "2023-06-18";
+export const END_DATE = "2023-06-17";
 
 export const START_DATE = "2022-11-18";
