da31058b5d8002700487d21e79d83ef6bda399e1
Jessy Lachapelle <jessy.lachapelle@akinox.com>
2023-11-30 22:02:44 +0800

    Update location

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,7 +2,20 @@ import { Data } from "../types";
 
 const data: Data[] = [
   {
-    from: "2023-11-07",
+    from: "2024-01-14",
+    timeZone: "Europe/Istanbul",
+    city: {
+      en: "Istanbul",
+      fr: "Istanbul",
+    },
+    country: {
+      en: "Turkey",
+      fr: "Turquie",
+    }
+  },
+  {
+    from: "2023-12-11",
+    to: "2024-01-13",
     timeZone: "Asia/Kuala_Lumpur",
     city: {
       en: "Kuala Lumpur",
@@ -14,216 +27,8 @@ const data: Data[] = [
     }
   },
   {
-    from: "2023-11-03",
-    to: "2023-11-06",
-    timeZone: "America/Vancouver",
-    city: {
-      en: "Vancouver",
-      fr: "Vancouver",
-    },
-    country: {
-      en: "Canada",
-      fr: "Canada",
-    }
-  },
-  {
-    from: "2023-09-27",
-    to: "2023-11-02",
-    timeZone: "America/Bogota",
-    city: {
-      en: "Medellin",
-      fr: "Medellin",
-    },
-    country: {
-      en: "Colombia",
-      fr: "Colombie",
-    }
-  },
-  {
-    from: "2023-09-15",
-    to: "2023-09-26",
-    timeZone: "America/Montreal",
-    city: {
-      en: "Sherbrooke",
-      fr: "Sherbrooke",
-    },
-    country: {
-      en: "Canada",
-      fr: "Canada",
-    }
-  },
-  {
-    from: "2023-08-18",
-    to: "2023-09-14",
-    timeZone: "America/Vancouver",
-    city: {
-      en: "Vancouver",
-      fr: "Vancouver",
-    },
-    country: {
-      en: "Canada",
-      fr: "Canada",
-    }
-  },
-  {
-    from: "2023-07-01",
-    to: "2023-08-17",
-    timeZone: "America/Montreal",
-    city: {
-      en: "Sherbrooke",
-      fr: "Sherbrooke",
-    },
-    country: {
-      en: "Canada",
-      fr: "Canada",
-    }
-  },
-  {
-    from: "2023-06-17",
-    to: "2023-07-01",
-    timeZone: "America/Vancouver",
-    city: {
-      en: "Vancouver",
-      fr: "Vancouver",
-    },
-    country: {
-      en: "Canada",
-      fr: "Canada",
-    }
-  },
-  {
-    from: "2023-06-12",
-    to: "2023-06-17",
-    timeZone: "Asia/Tokyo",
-    city: {
-      en: "Tokyo",
-      fr: "Tokyo",
-    },
-    country: {
-      en: "Japan",
-      fr: "Japon",
-    }
-  },
-  {
-    from: "2023-06-10",
-    to: "2023-06-12",
-    timeZone: "Asia/Tokyo",
-    city: {
-      en: "Kyoto",
-      fr: "Kyoto",
-    },
-    country: {
-      en: "Japan",
-      fr: "Japon",
-    }
-  },
-  {
-    from: "2023-06-06",
-    to: "2023-06-10",
-    timeZone: "Asia/Tokyo",
-    city: {
-      en: "Osaka",
-      fr: "Osaka",
-    },
-    country: {
-      en: "Japan",
-      fr: "Japon",
-    }
-  },
-  {
-    from: "2023-05-12",
-    to: "2023-06-16",
-    timeZone: "Asia/Tokyo",
-    city: {
-      en: "Tokyo",
-      fr: "Tokyo",
-    },
-    country: {
-      en: "Japan",
-      fr: "Japon",
-    }
-  },
-  {
-    from: "2023-04-10",
-    to: "2023-05-12",
-    timeZone: "Asia/Seoul",
-    city: {
-      en: "Seoul",
-      fr: "Séoul",
-    },
-    country: {
-      en: "South Korea",
-      fr: "Corée du Sud",
-    }
-  },
-  {
-    from: "2023-02-10",
-    to: "2023-04-10",
-    timeZone: "Asia/Kuala_Lumpur",
-    city: {
-      en: "Kuala Lumpur",
-      fr: "Kuala Lumpur",
-    },
-    country: {
-      en: "Malaysia",
-      fr: "Malaisie",
-    }
-  },
-  {
-    from: "2023-01-27",
-    to: "2023-02-10",
-    timeZone: "Asia/Saigon",
-    city: {
-      en: "Hanoi",
-      fr: "Hanoi",
-    },
-    country: {
-      en: "Vietnam",
-      fr: "Vietnam",
-    }
-  },
-  {
-    from: "2023-01-20",
-    to: "2023-01-27",
-    timeZone: "Asia/Saigon",
-    city: {
-      en: "Hoi An",
-      fr: "Hoi An",
-    },
-    country: {
-      en: "Vietnam",
-      fr: "Vietnam",
-    }
-  },
-  {
-    from: "2023-01-19",
-    to: "2023-01-20",
-    timeZone: "Asia/Saigon",
-    city: {
-      en: "Nha Trang",
-      fr: "Nha Trang",
-    },
-    country: {
-      en: "Vietnam",
-      fr: "Vietnam",
-    }
-  },
-  {
-    from: "2023-01-16",
-    to: "2023-01-19",
-    timeZone: "Asia/Saigon",
-    city: {
-      en: "Da Lat",
-      fr: "Da Lat",
-    },
-    country: {
-      en: "Vietnam",
-      fr: "Vietnam",
-    }
-  },
-  {
-    from: "2023-01-14",
-    to: "2023-01-16",
+    from: "2023-12-07",
+    to: "2023-12-10",
     timeZone: "Asia/Saigon",
     city: {
       en: "Ho Chi Minh City",
@@ -235,16 +40,16 @@ const data: Data[] = [
     }
   },
   {
-    from: "2022-12-31",
-    to: "2023-01-14",
-    timeZone: "Asia/Bangkok",
+    from: "2023-11-07",
+    to: "2023-12-06",
+    timeZone: "Asia/Kuala_Lumpur",
     city: {
-      en: "Chiang Mai",
-      fr: "Chiang Mai",
+      en: "Kuala Lumpur",
+      fr: "Kuala Lumpur",
     },
     country: {
-      en: "Thailand",
-      fr: "Thaïlande",
+      en: "Malaysia",
+      fr: "Malaisie",
     }
   },
 ];
