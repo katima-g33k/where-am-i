07ef0c299efe502a7a7550d9fdfb35ccc7cdb2d7
Jessy Lachapelle <jessy.lachapelle@akinox.com>
2024-01-22 19:23:25 +0300

    Update location

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,7 +2,46 @@ import { Data } from "../types";
 
 const data: Data[] = [
   {
+    from: "2024-01-29",
+    timeZone: "Europe/Budapest",
+    city: {
+      en: "Budapest",
+      fr: "Budapest",
+    },
+    country: {
+      en: "Hungary",
+      fr: "Hongrie",
+    }
+  },
+  {
+    from: "2024-01-29",
+    to: "2024-02-02",
+    timeZone: "Europe/Bucharest",
+    city: {
+      en: "Bucharest",
+      fr: "Bucharest",
+    },
+    country: {
+      en: "Romania",
+      fr: "Roumanie",
+    }
+  },
+  {
+    from: "2024-01-24",
+    to: "2024-01-28",
+    timeZone: "Europe/Sofia",
+    city: {
+      en: "Sofia",
+      fr: "Sofia",
+    },
+    country: {
+      en: "Bulgaria",
+      fr: "Gulgarie",
+    }
+  },
+  {
     from: "2024-01-14",
+    to: "2024-01-23",
     timeZone: "Europe/Istanbul",
     city: {
       en: "Istanbul",
