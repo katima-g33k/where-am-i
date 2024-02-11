12aa190a9b2d5173c660b61999a714ff516ee463
Jessy Lachapelle <jessy.lachapelle@akinox.com>
2024-02-11 09:31:08 +0100

    Update location

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,47 +2,8 @@ import { Data } from "../types";
 
 const data: Data[] = [
   {
-    from: "2024-01-24",
-    to: "2024-01-28",
-    timeZone: "Europe/Sofia",
-    city: {
-      en: "Sofia",
-      fr: "Sofia",
-    },
-    country: {
-      en: "Bulgaria",
-      fr: "Bulgarie",
-    }
-  },
-  {
-    from: "2024-01-29",
-    to: "2024-02-02",
-    timeZone: "Europe/Bucharest",
-    city: {
-      en: "Bucharest",
-      fr: "Bucharest",
-    },
-    country: {
-      en: "Romania",
-      fr: "Roumanie",
-    }
-  },
-  {
-    from: "2024-02-03",
-    to: "2024-02-09",
-    timeZone: "Europe/Budapest",
-    city: {
-      en: "Budapest",
-      fr: "Budapest",
-    },
-    country: {
-      en: "Hungary",
-      fr: "Hongrie",
-    }
-  },
-  {
     from: "2024-02-10",
-    to: "2024-02-11",
+    to: "2024-02-12",
     timeZone: "Europe/Bratislava",
     city: {
       en: "Bratislava",
@@ -64,58 +25,6 @@ const data: Data[] = [
     country: {
       en: "Czech Republic",
       fr: "République tchèque",
-    }
-  },
-  {
-    from: "2024-01-14",
-    to: "2024-01-23",
-    timeZone: "Europe/Istanbul",
-    city: {
-      en: "Istanbul",
-      fr: "Istanbul",
-    },
-    country: {
-      en: "Turkey",
-      fr: "Turquie",
-    }
-  },
-  {
-    from: "2023-12-11",
-    to: "2024-01-13",
-    timeZone: "Asia/Kuala_Lumpur",
-    city: {
-      en: "Kuala Lumpur",
-      fr: "Kuala Lumpur",
-    },
-    country: {
-      en: "Malaysia",
-      fr: "Malaisie",
-    }
-  },
-  {
-    from: "2023-12-07",
-    to: "2023-12-10",
-    timeZone: "Asia/Saigon",
-    city: {
-      en: "Ho Chi Minh City",
-      fr: "Ho Chi Minh City",
-    },
-    country: {
-      en: "Vietnam",
-      fr: "Vietnam",
-    }
-  },
-  {
-    from: "2023-11-07",
-    to: "2023-12-06",
-    timeZone: "Asia/Kuala_Lumpur",
-    city: {
-      en: "Kuala Lumpur",
-      fr: "Kuala Lumpur",
-    },
-    country: {
-      en: "Malaysia",
-      fr: "Malaisie",
     }
   },
 ];
