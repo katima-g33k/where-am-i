ea822e8be6b5d5569e205878d0fb88f0e6fae2f3
Jessy Lachapelle <jessy.lachapelle@akinox.com>
2024-02-20 14:19:07 +0100

    Update location

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,29 +2,106 @@ import { Data } from "../types";
 
 const data: Data[] = [
   {
-    from: "2024-02-10",
-    to: "2024-02-12",
-    timeZone: "Europe/Bratislava",
+    from: "2024-02-20",
+    to: "2024-02-26",
+    timeZone: "Europe/Amsterdam",
     city: {
-      en: "Bratislava",
-      fr: "Bratislava",
+      en: "Amsterdam",
+      fr: "Amsterdam",
     },
     country: {
-      en: "Slovakia",
-      fr: "Slovaquie",
+      en: "Netherlands",
+      fr: "Pays-Bas",
     }
   },
   {
-    from: "2024-02-12",
-    to: "2024-02-19",
-    timeZone: "Europe/Prague",
+    from: "2024-02-27",
+    to: "2024-02-28",
+    timeZone: "Europe/Brussels",
     city: {
-      en: "Prague",
-      fr: "Prague",
+      en: "Brussels",
+      fr: "Bruxelles",
     },
     country: {
-      en: "Czech Republic",
-      fr: "République tchèque",
+      en: "Belgium",
+      fr: "Belgique",
+    }
+  },
+  {
+    from: "2024-02-29",
+    to: "2024-02-29",
+    timeZone: "Europe/Luxembourg",
+    city: {
+      en: "Luxembourg",
+      fr: "Luxembourg",
+    },
+    country: {
+      en: "Luxembourg",
+      fr: "Luxembourg",
+    }
+  },
+  {
+    from: "2024-03-01",
+    to: "2024-03-02",
+    timeZone: "Europe/Paris",
+    city: {
+      en: "Dijon",
+      fr: "Dijon",
+    },
+    country: {
+      en: "France",
+      fr: "France",
+    }
+  },
+  {
+    from: "2024-03-03",
+    to: "2024-03-03",
+    timeZone: "Europe/Madrid",
+    city: {
+      en: "Barcelona",
+      fr: "Barcelone",
+    },
+    country: {
+      en: "Spain",
+      fr: "Espagne",
+    }
+  },
+  {
+    from: "2024-03-04",
+    to: "2024-03-12",
+    timeZone: "Europe/Madrid",
+    city: {
+      en: "Valencia",
+      fr: "Valence",
+    },
+    country: {
+      en: "Spain",
+      fr: "Espagne",
+    }
+  },
+  {
+    from: "2024-03-13",
+    to: "2024-03-18",
+    timeZone: "Europe/Lisbon",
+    city: {
+      en: "Lisbon",
+      fr: "Lisbonne",
+    },
+    country: {
+      en: "Portugal",
+      fr: "Portugal",
+    }
+  },
+  {
+    from: "2024-03-19",
+    timeZone: "America/Montreal",
+    city: {
+      en: "Sherbrooke",
+      fr: "Sherbrooke",
+    },
+    country: {
+      en: "Canada",
+      fr: "Canada",
     }
   },
 ];
